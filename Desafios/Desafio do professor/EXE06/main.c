a7cc3377d5742727a82bdbc3ff48438b18fafdfc
Igor Cechinato de Lima <156817223+IgorCdeLima@users.noreply.github.com>
2024-03-14 11:22:03 -0300

    a

---
+++ Desafios/Desafio do professor/EXE06/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int p, u, r, n, cont, quant;
+
+    printf("TErmos de uma P.A\n");
+    printf("\nPrimeiro termo: "); scanf("%d", &p);
+    printf("\nUltimo termo: "); scanf("%d", &u);
+    printf("\nRazao: "); scanf("%d", &r);
+
+    for (p == p; p == u; p++)
+    {
+        p = p + r;
+        printf("%d", p);
+    }
+    return 0;
+}
